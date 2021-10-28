2f7c439fca809b21fc6fbe3450f6fdee7ed3bf7c
AlbertoPimentel <albertopimentel04@gmail.com>
2021-10-27 19:03:32 -0500

    discount, arrivals and footer responsive mode completed

--- assets/js/main.js
+++ assets/js/main.js
@@ -49,9 +49,43 @@ function scrollHeader() {
 }
 window.addEventListener("scroll", scrollHeader);
 /*=============== NEW SWIPER ===============*/
-
+let newSwiper = new Swiper(".new-swiper", {
+  centeredSlides: true,
+  slidesPerView: "auto",
+  spaceBetween: 16,
+  loop: true,
+});
 /*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
 
-/*=============== SHOW SCROLL UP ===============*/
+const sections = document.querySelectorAll("section[id]");
 
+function scrollActive() {
+  const scrollY = window.pageYOffset;
+
+  sections.forEach((current) => {
+    const sectionHeight = current.offsetHeight;
+    const sectionTop = current.offsetTop - 50;
+    sectionId = current.getAttribute("id");
+
+    if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
+      document
+        .querySelector(".nav__menu a[href*=" + sectionId + "]")
+        .classList.add("active-link");
+    } else {
+      document
+        .querySelector(".nav__menu a[href*=" + sectionId + "]")
+        .classList.remove("active-link");
+    }
+  });
+}
+window.addEventListener("scroll", scrollActive);
+
+/*=============== SHOW SCROLL UP ===============*/
+function scrollUp() {
+  const scrollUp = document.getElementById("scrollup");
+  // When the scroll is higher than 460 viewport height, add the show-scroll class to the a tag with the scroll-top class
+  if (this.scrollY >= 460) scrollUp.classList.add("show-scroll");
+  else scrollUp.classList.remove("show-scroll");
+}
+window.addEventListener("scroll", scrollUp);
 /*=============== SCROLL REVEAL ANIMATION ===============*/
